06de3a80303347f3e5dfdb2547b38326150b7c1d
kavithashanmugan <kavitha.shanmugan@gmail.com>
2021-08-07 13:42:33 -0600

    learning java

--- 03_04/RandomNumbers/src/randomnumbers/RandomNumbers.java
+++ 03_04/RandomNumbers/src/randomnumbers/RandomNumbers.java
@@ -3,7 +3,8 @@
  * It randomly generates two die values 100 times
  * and counts how many times we roll doubles
  */
-package randomnumbers;
+//package randomnumbers;
+import java.util.Random;
 
 /**
  *
@@ -15,7 +16,17 @@ public class RandomNumbers {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+       double die1,die2,countDoubles=0;
+       Random rand = new Random();
+        for(int i=0;i<100;i++){
+            die1 = rand.nextInt(6)+1;
+            die2 = rand.nextInt(6)+1;
+            if(die1==die2){
+                countDoubles++;
+            } 
+        }
+
+        System.out.println("Count is"+countDoubles);
     }
     
 }
